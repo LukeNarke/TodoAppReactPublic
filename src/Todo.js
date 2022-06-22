1b21c1ce4f75a7e6b0fc0e1b956effe2e07f4c93
Luke Narke <74273366+LukeNarke@users.noreply.github.com>
2022-06-22 10:04:27 -0500

    Update Todo.js

--- src/Todo.js
+++ src/Todo.js
@@ -18,7 +18,7 @@ const useStyles = makeStyles((theme) => ({
     // position: "center",
     width: 600,
     backgroundColor: theme.palette.background.paper,
-    // border: "2px solid #000",
+    border: "2px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
   },
